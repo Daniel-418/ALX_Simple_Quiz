27631c0660d329d47e61256d93c03b2f5f4cca02
Daniel Komolafe <daniel@komolafe.dev>
2025-11-29 14:42:59 -0600

    finished task

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function checkAnswer() {
+  const correctAnswer = "4";
+  const userAnswer = document.querySelector("[name=quiz]").value;
+  const feedback = document.getElementById("feedback");
+
+  if (userAnswer == correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  }
+  else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
+}
+
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
